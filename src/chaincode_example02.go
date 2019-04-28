ca2f969eb729beb7af5f070abd6b2e4ad3073cba
Iris Baron <32202824+irisbaron@users.noreply.github.com>
2019-04-28 15:44:03 -0400

    Update chaincode_example02.go

--- src/chaincode_example02.go
+++ src/chaincode_example02.go
@@ -128,8 +128,8 @@ func (t *SimpleChaincode) invoke(stub shim.ChaincodeStubInterface, args []string
 	if err != nil {
 		return shim.Error("Invalid transaction amount, expecting a integer value")
 	}
-	Aval = Aval - 2*X 
-	Bval = Bval + 2*X 
+	Aval = Aval - X 
+	Bval = Bval + X 
 	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
 
 	// Write the state back to the ledger
